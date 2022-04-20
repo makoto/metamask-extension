e94f9531d81d480a78eb7964201739cd3fee9163
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-04-20 11:12:17 -0400

    removing TOKEN_DETECTION_V2 feature flag

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -747,8 +747,8 @@ function getAllowedNotificationIds(state) {
     7: false,
     8: supportsWebHid && currentKeyringIsLedger && currentlyUsingLedgerLive,
     9: getIsMainnet(state),
-    10: Boolean(process.env.TOKEN_DETECTION_V2),
-    11: Boolean(process.env.TOKEN_DETECTION_V2),
+    10: true,
+    11: true,
     12: true,
   };
 }
